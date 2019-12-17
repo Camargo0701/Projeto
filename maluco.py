ef553837805877c13136f0a14796fa4bd15ead40
unknown <matheus.bargouzinho@hotmail.com>
2019-12-17 20:48:10 -0200

    first commit

---
+++ maluco.py
@@ -0,0 +1,19 @@
+n1=input(str('Digite um texto: '))
+print(n1)
+print(n1*7)
+print(n1*7)
+print(n1*7)
+print(n1*7)
+print(n1*7)
+print(n1*7)
+print(n1*7)
+
+#podemos também fazer desse jeito
+
+
+item []
+degrau=int(input('Número de degrais: '))
+caracter=input('Escolha o caractere')
+for item in range(1, degrau+1):
+    print(caracter*degrau)
+    item+=1
---
+++ resolucao.py
@@ -0,0 +1,33 @@
+x = ('Luar')
+
+x = x.replace("a", "@")
+x = x.replace("e", "&")
+x = x.replace("i", "!")
+x = x.replace("o", "#")
+x = x.replace("u", "*")
+
+print(x)
+
+p = ('Bocejo')
+
+p = p.replace("a", "@")
+p = p.replace("e", "&")
+p = p.replace("i", "!")
+p = p.replace("o", "#")
+p = p.replace("u", "*")
+
+print(p)
+
+
+t = ('tranquilo')
+
+t = t.replace("a", "@")
+t = t.replace("e", "&")
+t = t.replace("i", "!")
+t = t.replace("o", "#")
+t = t.replace("u", "*")
+
+print(t)
+
+
+
